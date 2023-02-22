922ddb78ae4a8ea3afb80a155dc289331c66cd7b
Eric Y Chang <ericyc@protonmail.com>
2023-02-21 20:28:25 -0600

    FAQ button color; nonscrollable elem

--- js/script.js
+++ js/script.js
@@ -4,6 +4,16 @@ scrollableElement.addEventListener('wheel', (ev) => {
     scrollableElement.scrollLeft += (ev.deltaY + ev.deltaX);
 });
 
+const el = document.getElementsByClassName('nonScrollableElement');
+for (var i = 0; i < el.length; i++) {
+  var e = el[i];
+  e.addEventListener('wheel', (ev) => {
+    //ev.preventDefault();  // stop scrolling in another direction
+    scrollableElement.scrollLeft -= (ev.deltaY + ev.deltaX);
+    e.scrollTop += (ev.deltaY + ev.deltaX);
+  }
+)}
+
 function smoothScroll(whereToScroll) {
   const element = document.getElementById(whereToScroll);
   element.scrollIntoView({behavior: "smooth", inline: "center"});
