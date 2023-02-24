882fed021652f1743ae34a8583eb1144d4e63197
Eric Y Chang <ericyc@protonmail.com>
2023-02-24 15:46:56 -0600

    Finish nonscrollableElement, move faq/schedule

--- js/script.js
+++ js/script.js
@@ -4,15 +4,32 @@ scrollableElement.addEventListener('wheel', (ev) => {
     scrollableElement.scrollLeft += (ev.deltaY + ev.deltaX);
 });
 
+/* Allow up/down scroll only on special non-scrollable elements. 
+ * After scrolling entirely through the element, continue horizontal scroll.
+ */
 const el = document.getElementsByClassName('nonScrollableElement');
 for (var i = 0; i < el.length; i++) {
   var e = el[i];
   e.addEventListener('wheel', (ev) => {
-    //ev.preventDefault();  // stop scrolling in another direction
-    scrollableElement.scrollLeft -= (ev.deltaY + ev.deltaX);
-    e.scrollTop += (ev.deltaY + ev.deltaX);
+    if (hasScrollbar(e) && !((atBottom(e) && ev.deltaY > 0) ||(atTop(e) && ev.deltaY < 0))) {
+      scrollableElement.scrollLeft -= (ev.deltaY + ev.deltaX);
+      e.scrollTop += (ev.deltaY + ev.deltaX);
+    }
   }
 )}
+
+function hasScrollbar(element) {
+  return element.scrollHeight > element.clientHeight;
+}
+
+function atBottom(element) {
+  // -2 to give margin, since sometimes it isn't entirely at bottom for some reason.
+  return (element.scrollTop >= element.scrollHeight - element.clientHeight - 2);
+}
+
+function atTop(element) {
+  return element.scrollTop == 0;
+}
 
 function smoothScroll(whereToScroll) {
   const element = document.getElementById(whereToScroll);
